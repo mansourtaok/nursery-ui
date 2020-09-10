8795adc7033145ec8cc3b3e89593967f49dd9e4f
Mansour <mansour.taok@gmail.com>
2020-09-10 22:25:33 +0300

    nusrsery skeleton

---
+++ src/app/main/ui/nursery.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { UIFormsModule } from 'app/main/ui/forms/forms.module';
+
+
+@NgModule({
+    imports: [
+        UIFormsModule,
+    ]
+})
+export class NureryModule
+{
+    
+}
